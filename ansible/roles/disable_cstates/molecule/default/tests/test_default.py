c4a0c0ffdb33ebcf705bfc97e0377ef18e65ac95
Mathis Dlmr <mathis.dlmr@etik.com>
2026-08-08 08:35:58 +0200

    chore(ansible: molecule: c_states): we can remove molecule for disabmle_cstates role because it mostly depends on hardware and magical things

--- ansible/roles/disable_cstates/molecule/default/tests/test_default.py
+++
@@ -1,6 +0,0 @@
-# molecule/default/tests/test_default.py
-# Testinfra checks that the role under test has been applied correctly.
-
-def test_cstates_are_disabled(host):
-    grub_file = host.file("/etc/default/grub")
-    assert grub_file.contains('GRUB_CMDLINE_LINUX_DEFAULT="quiet splash processor.max_cstate=1"')
